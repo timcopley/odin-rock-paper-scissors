ea903ef76aa113a7836724891b858deddd56a250
Tim Copley <249600077+timcopley@users.noreply.github.com>
2025-12-22 20:12:26 -0500

    updated getComputerChoice to return text

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function getComputerChoice() {
     let randNum = Math.floor((Math.random() * 3) + 1);
 
-    return randNum;
+    switch (randNum) {
+        case 1: return "rock";
+        case 2: return "paper";
+        case 3: return "scissors";
+    }
 }
 
 console.log(getComputerChoice());
