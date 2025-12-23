8e8f8679d1edfa552118ddfe1abcdfa7bf2eafed
Tim Copley <249600077+timcopley@users.noreply.github.com>
2025-12-22 20:43:48 -0500

    created playRound function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let randNum = Math.floor((Math.random() * 3) + 1);
 
@@ -12,4 +14,42 @@ function getHumanChoice() {
     return prompt("Choose rock, paper, or scissors: ");
 }
 
-console.log(getHumanChoice());
+function playRound (humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    console.log(humanChoice + " " + computerChoice);
+    if (humanChoice === computerChoice) {
+        console.log(`Both sides threw ${humanChoice} -- Round is a draw.`);
+    }
+    else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
+            computerScore += 1;
+        }
+        else { 
+            console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
+            humanScore += 1;
+        }
+    }
+    else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
+            computerScore += 1;
+        }
+        else { 
+            console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
+            humanScore += 1;
+        }
+    }
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
+            computerScore += 1;
+        }
+        else { 
+            console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
+            humanScore += 1;
+        }
+    }        
+}
+
+playRound(getHumanChoice(), getComputerChoice());
