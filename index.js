58b9b6dfe3dcd1005b9171247f3fd3fffe61b60b
Tim Copley <249600077+timcopley@users.noreply.github.com>
2026-01-07 10:13:07 -0500

    Refactored to use buttons to run the playRound function.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+let humanScore = 0, computerScore = 0;
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => { playRound('rock')});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => { playRound('paper')});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => { playRound('scissors')});
+
 function getComputerChoice() {
     let randNum = Math.floor((Math.random() * 3) + 1);
 
@@ -12,46 +23,51 @@ function getHumanChoice() {
     return prompt("Choose rock, paper, or scissors: ");
 }
 
+function playRound (humanChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    const computerChoice = getComputerChoice();
+
+    const results = document.querySelector('#results');
+
+    if (humanChoice === computerChoice) {
+        console.log(`Both sides threw ${humanChoice} -- Round is a draw.`);
+    }
+    else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
+            computerScore += 1;
+        }
+        else { 
+            console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
+            humanScore += 1;
+        }
+    }
+    else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
+            computerScore += 1;
+        }
+        else { 
+            console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
+            humanScore += 1;
+        }
+    }
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
+            computerScore += 1;
+        }
+        else { 
+            console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
+            humanScore += 1;
+        }
+    }        
+}   
+
 function playGame(roundsToPlay) {
-    let humanScore = 0, computerScore = 0;
 
-    function playRound (humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
 
-        if (humanChoice === computerChoice) {
-            console.log(`Both sides threw ${humanChoice} -- Round is a draw.`);
-        }
-        else if (humanChoice === "rock") {
-            if (computerChoice === "paper") {
-                console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
-                computerScore += 1;
-            }
-            else { 
-                console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
-                humanScore += 1;
-            }
-        }
-        else if (humanChoice === "paper") {
-            if (computerChoice === "scissors") {
-                console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
-                computerScore += 1;
-            }
-            else { 
-                console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
-                humanScore += 1;
-            }
-        }
-        else if (humanChoice === "scissors") {
-            if (computerChoice === "rock") {
-                console.log(`You lose: ${computerChoice} beats ${humanChoice}!`)
-                computerScore += 1;
-            }
-            else { 
-                console.log(`You win: ${humanChoice} beats ${computerChoice}!`)
-                humanScore += 1;
-            }
-        }        
-    }    
+ 
 
     for (let i = 1; i < roundsToPlay + 1; i++) {
         console.log(`Round ${i} of ${roundsToPlay}!`)
@@ -66,5 +82,3 @@ function playGame(roundsToPlay) {
     }
     else { console.log(`You lose! Final score: ${humanScore} to ${computerScore}`) }
 }
-
-playGame(5);
